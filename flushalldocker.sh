a57dd4654ed2ffc3cf9bc3224619c50bdbd38706
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-02 18:54:37 +0200

    Update flushalldocker.sh

--- flushalldocker.sh
+++ flushalldocker.sh
@@ -23,8 +23,6 @@ rm -rf /etc/ceph \
 for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
 
 rm -f /var/lib/containerd/io.containerd.metadata.v1.bolt/meta.db
-sudo systemctl restart containerd
-sudo systemctl restart docker
 
 IPTABLES="/sbin/iptables"
 cat /proc/net/ip_tables_names | while read table; do
